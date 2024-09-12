ec9bbb8473fd676e51a4fb5151a729572440d628
jason <420994592@qq.com>
2024-09-12 09:13:07 +0800

    增加模型

--- app/models/policy.go
+++ app/models/policy.go
@@ -8,6 +8,13 @@ type Policy struct {
 	SoftDeletes
 }
 
+type PolicyAuth struct {
+	ID
+	PolicyId   int64 `json:"policy_id"`
+	TargetType int8  `json:"target_type" gorm:"comment:0用户1角色2部门"`
+	TargetId   int64 `json:"target_id"`
+}
+
 type PolicyResource struct {
 	ID
 	PolicyId          int64 `json:"policy_id"`
@@ -16,4 +23,12 @@ type PolicyResource struct {
 	Effect            int8  `json:"effect" gorm:"comment:效果0/1"`
 	Timestamps
 	SoftDeletes
+}
+
+type PolicyResourceItem struct {
+	ID
+	PolicyId            int64  `json:"policy_id"`
+	PolicyResourceId    int64  `json:"policy_resouce_id"`
+	ResourceItemId      int64  `json:"resource_item_id"`
+	ResourceItemActions string `json:"resource_item_actions" gorm:"size:255;not null"`
 }
